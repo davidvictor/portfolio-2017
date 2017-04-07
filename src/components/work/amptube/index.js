60bfe55a341d35809315d78bf0c97188ca65091a
David Victor <david@lionhou.se>
2017-04-06 23:06:16 -0700

    amptube hero, copy adjustment

--- src/components/work/amptube/index.js
+++ src/components/work/amptube/index.js
@@ -16,8 +16,8 @@ const Amptube = () => {
 				roles={['strategy','branding','ui','ux','code']}
 				description="Amptube is a discovery tool that allows up and coming creators the opportunity to earn featured placement on an established creator’s page by sharing videos with their social networks."
 				contribution="Food truck pour-over microdosing, four dollar toast messenger bag pug flexitarian flannel church-key etsy. Mustache tattooed art party small batch narwhal, live-edge green juice pickled pug offal meh ugh. Mlkshk VHS skateboard, chambray meh PBR&B fixie lyft coloring book typewriter gastropub neutra hell of live-edge taxidermy."
-				bg={{src:`${assetUrl}/amptube/hero.png`,mobile:`${assetUrl}/amptube/hero.png`}}
-				logo={{src: `${assetUrl}/amptube/mark.svg`, width: false}}>
+				bg={{src:`${assetUrl}/hero/at-hero.jpg`,mobile:`${assetUrl}/hero/at-hero-mobile.jpg`}}
+				logo={{src: `${assetUrl}/amptube/lockup-horizontal-white.svg`, width: '260px'}}>
 			
 			</Project>
 		</div>
